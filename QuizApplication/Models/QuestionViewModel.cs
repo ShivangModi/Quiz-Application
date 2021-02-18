2b15fb44d0e51eb03b75b58fa2c3ec4f92d3a17d
ShivangModi <68642058+ShivangModi@users.noreply.github.com>
2021-02-18 11:40:14 +0530

    commit

---
+++ QuizApplication/Models/QuestionViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace QuizApplication.Models
+{
+    public class Answers
+    {
+        public long id { get; set; }
+        public string answer { get; set; }
+    }
+
+    public class QuestionViewModel : Answers
+    {
+        public string text { get; set; }
+
+        public string option1 { get; set; }
+
+        public string option2 { get; set; }
+
+        public string option3 { get; set; }
+
+        public string option4 { get; set; }
+
+        public short type { get; set; }
+
+        public int examId { get; set; }
+    }
+}
