e7cd223556a8ef0eaa75056ec7f609db7fd0d69b
ShivangModi <68642058+ShivangModi@users.noreply.github.com>
2021-02-21 20:28:59 +0530

    Commit

---
+++ WebAPIProject_CE070_CE079_CE130/QuizApp/QuizApplication/Controllers/ExamController.cs
@@ -0,0 +1,23 @@
+﻿using QuizApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace QuizApplication.Controllers
+{
+    public class ExamController : ApiController
+    {
+        db_QUIZEntities db = new db_QUIZEntities();
+        List<TBL_Exam> li = null;
+
+        [HttpGet]
+        public HttpResponseMessage GetAllExams(long id)
+        {
+            li = db.TBL_Exam.Where(x => x.examCreatedBy == id).ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, li);
+        }
+    }
+}
